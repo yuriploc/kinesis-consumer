bcef452972309b8728b8bee254d37e088c1c509d
Yuri Oliveira <soaresyuri@gmail.com>
2020-01-09 17:23:37 -0300

    lint: using black

--- kinesis_consumer.py
+++ kinesis_consumer.py
@@ -8,62 +8,63 @@ import requests
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
-URL = os.environ['BC_HOST']
-BC_KEY = os.environ['BC_KEY']
-params = {'couponId': None}
-headers = {'Authorization': BC_KEY}
+URL = os.environ["BC_HOST"]
+BC_KEY = os.environ["BC_KEY"]
+params = {"couponId": None}
+headers = {"Authorization": BC_KEY}
 
 # Convert bytes to str, if required
 def convert_str(s):
-  return s.decode('utf-8') if isinstance(s, bytes) else s
+    return s.decode("utf-8") if isinstance(s, bytes) else s
+
 
 def get_coupon_complete_data(coupon_id):
-  logger.info('## COUPON ID TO GET')
-  logger.info(coupon_id)
-  params['couponId'] = coupon_id
-  req = requests.get(URL, params=params, headers=headers)
+    logger.info("## COUPON ID TO GET")
+    logger.info(coupon_id)
+    params["couponId"] = coupon_id
+    req = requests.get(URL, params=params, headers=headers)
 
-  logger.info('## DATA FROM BC API')
-  logger.info(req.json())
+    logger.info("## DATA FROM BC API")
+    logger.info(req.json())
 
-  return req.json()
+    return req.json()
 
 
 def lambda_handler(event, context):
-    for record in event['Records']:
-      # Kinesis data is base64 encoded so decode here
-      data_b = base64.b64decode(record["kinesis"]["data"])
-      data_str = convert_str(data_b)
-      data = json.loads(data_str)
-      logger.info('## DATA')
-      logger.info(data)
+    for record in event["Records"]:
+        # Kinesis data is base64 encoded so decode here
+        data_b = base64.b64decode(record["kinesis"]["data"])
+        data_str = convert_str(data_b)
+        data = json.loads(data_str)
+        logger.info("## DATA")
+        logger.info(data)
 
-      if data['table'] == 'coupon':
-        logger.info('## COUPON TABLE')
-        operation_type = data['type']
-        logger.info(operation_type)
+        if data["table"] == "coupon":
+            logger.info("## COUPON TABLE")
+            operation_type = data["type"]
+            logger.info(operation_type)
 
-        coupon_data = None
+            coupon_data = None
 
-        if operation_type == 'WriteRowsEvent':
-          logger.info('## WRITE ROWS EVENT')
-          row = data['row']['values']
+            if operation_type == "WriteRowsEvent":
+                logger.info("## WRITE ROWS EVENT")
+                row = data["row"]["values"]
 
-          coupon_id = row['id']
-          coupon_data = get_coupon_complete_data(coupon_id)
+                coupon_id = row["id"]
+                coupon_data = get_coupon_complete_data(coupon_id)
 
+            if operation_type == "UpdateRowsEvent":
+                logger.info("## UPDATE ROWS EVENT")
+                # before_values = data['row']['before_values']
+                after_values_row = data["row"]["after_values"]
 
-        if operation_type == 'UpdateRowsEvent':
-          logger.info('## UPDATE ROWS EVENT')
-          # before_values = data['row']['before_values']
-          after_values_row = data['row']['after_values']
+                coupon_id = after_values_row["id"]
+                coupon_data = get_coupon_complete_data(coupon_id)
 
-          coupon_id = after_values_row['id']
-          coupon_data = get_coupon_complete_data(coupon_id)
-
-        # TODO: call Financial API to save/update the coupon complete data
+            # TODO: call Financial API to save/update the coupon complete data
 
     return {
-        'statusCode': 200,
-        'body': json.dumps('OK')
+        "statusCode": 200,
+        "headers": {"Content-Type": "application/json"},
+        "body": "",
     }
